cd0271e22bb2a0be619e93570e2b6858cb2a29ca
Vaztonyo <tony.dewet7@gmail.com>
2017-06-13 11:58:49 +0200

    updated db connection in index.js

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ var app = express();
 var expressHandlebars = require('express-handlebars');
 var bodyParser = require('body-parser');
 
-var database_connection = require('./database-connection')
+var mongoURL = process.env.MONGO_DB_URL || "mongodb://hackathonT1:12345@ds119081.mlab.com:19081/jumo_customers";
+
+var database_connection = require('./database-connection');
+
+//call database_connection function
+//pass in the mongoURL connection
+var dbconnection = database_connection(mongoURL);
+
 var models = require('./model');
 
 var server = app.listen(3000, function() {
