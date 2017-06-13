91eebf1e58e2de8cde9b3c408bd7f9b0e6b9fc4c
GarethW1994 <williams.gareth0@gmail.com>
2017-06-13 11:44:54 +0200

    added database-connection and model

---
+++ model.js
@@ -0,0 +1,47 @@
+//require mongoose
+var mongoose = require('mongoose');
+var assert = require('assert');
+
+module.exports = function () {
+	const userData = function () {
+		const user_schema = new mongoose.Schema({
+			userID: {
+				type: String,
+				required: true
+			},
+
+			userName: {
+				type: String
+			},
+
+			userSurname: {
+				type: String
+			},
+
+			city: {
+				type: String
+			},
+
+			about: {
+				type: String
+			}
+
+		});
+
+		//declare the unique values
+		user_schema.index({
+			userID: 1
+		}, {
+			unique: true
+		});
+
+		var users = mongoose.model('users', user_schema);
+
+		return users;
+	}
+
+
+	return {
+		usersData
+	};
+}
